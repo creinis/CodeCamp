54e965e2882ab94f572fa8ccdea80841daa40f94
creinis <115181527+creinis@users.noreply.github.com>
2023-08-11 16:56:28 +0200

    students info finished

--- 05_RelationalDatabaseCertification/Students_SQL_01/students_info.sh
+++ 05_RelationalDatabaseCertification/Students_SQL_01/students_info.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 # Info about my computer science students from students database
@@ -29,5 +28,14 @@ echo -e "\nAverage GPA of all students rounded to two decimal places:"
 echo "$($PSQL "SELECT ROUND(AVG(gpa), 2) FROM students")"
 
 echo -e "\nMajor ID, total number of students in a column named 'number_of_students', and average GPA rounded to two decimal places in a column name 'average_gpa', for each major ID in the students table having a student count greater than 1:"
+echo "$($PSQL "SELECT major_id, COUNT(*) AS number_of_students, ROUND(AVG(gpa), 2) AS average_gpa FROM students GROUP BY major_id HAVING COUNT(*) > 1")"
 
+echo -e "\nList of majors, in alphabetical order, that either no student is taking or has a student whose first name contains a case insensitive 'ma':"
+echo "$($PSQL "SELECT major FROM students FULL JOIN majors ON students.major_id = majors.major_id WHERE major IS NOT NULL AND (student_id IS NULL OR first_name ILIKE '%ma%') ORDER BY major")"
+
+echo -e "\nList of unique courses, in reverse alphabetical order, that no student or 'Obie Hilpert' is taking:"
+echo "$($PSQL "SELECT DISTINCT(course) FROM students RIGHT JOIN majors USING(major_id) INNER JOIN majors_courses USING(major_id) INNER JOIN courses USING(course_id) WHERE (first_name = 'Obie' AND last_name = 'Hilpert') OR student_id IS NULL ORDER BY course DESC")"
+
+echo -e "\nList of courses, in alphabetical order, with only one student enrolled:"
+echo "$($PSQL "SELECT course FROM students INNER JOIN majors_courses USING(major_id) INNER JOIN courses USING(course_id) GROUP BY course HAVING COUNT(student_id) = 1 ORDER BY course")"
 
