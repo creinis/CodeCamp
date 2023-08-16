87a5a0ee8e87407670040778cda34e251af1c61c
creinis <115181527+creinis@users.noreply.github.com>
2023-08-16 22:47:13 +0200

    salon finhished

--- 05_RelationalDatabaseCertification/SalonAppointmentScheduler/salon.sh
+++ 05_RelationalDatabaseCertification/SalonAppointmentScheduler/salon.sh
@@ -6,6 +6,9 @@ echo -e "\n~~~~~~~~~~ MY SALON ~~~~~~~~~~\n"
 
 echo -e "Welcome to MY SALON, how can I help you?\n"
 
+# get available services
+AVAILABLE_SERVICES=$($PSQL "SELECT service_id, name FROM services ORDER BY service_id;")
+
 # first prompt
 MAIN_MENU() {
 
@@ -14,8 +17,7 @@ MAIN_MENU() {
     echo -e "\n$1"
   fi
 
-  # get available services
-  AVAILABLE_SERVICES=$($PSQL "SELECT service_id, name FROM services ORDER BY service_id")
+# available services Menu
   echo "$AVAILABLE_SERVICES" | while read SERVICE_ID BAR NAME
   do
     echo "$SERVICE_ID) $NAME"
@@ -23,17 +25,21 @@ MAIN_MENU() {
 
   read SERVICE_ID_SELECTED
   
-  if [[ ! $SERVICE_ID_SELECTED =~ ^[0-4]+$ ]]
-    then
-      echo "I could not find that service. What would you like today?"
-      MAIN_MENU 
-    fi
-    if [[ -z $SERVICE_ID_SELECTED ]]
-    then
-      echo "I could not find that service. What would you like today?"
-      MAIN_MENU
-   fi
-  
+  case $SERVICE_ID_SELECTED in
+  [1-4]) PASS ;;
+  *) MAIN_MENU "I could not find that service. What would you like today?" ;;
+  esac
+}
+  #if [[ ! $SERVICE_ID_SELECTED =~ ^[0-9]+$ ]]
+   # then
+       
+    #fi
+    #if [[ -z $SERVICE_ID_SELECTED ]]
+    #then
+     # echo -e "\nI could not find that service. What would you like today?"
+     # MAIN_MENU
+   # fi
+  PASS() {
 # service selected
 SERVICE_ID_SELECTED=$($PSQL "SELECT service_id FROM services WHERE service_id = '$SERVICE_ID_SELECTED'")
 
@@ -66,16 +72,11 @@ read SERVICE_TIME
 # insert appointment
 INSERT_APPOINTMENT=$($PSQL "INSERT INTO appointments(service_id, customer_id, time) VALUES('$SERVICE_ID_SELECTED', '$CUSTOMER_ID', '$SERVICE_TIME')")
 
-# get appointment_id
-# APPOINTMENT_ID=$($PSQL "SELECT service_id, customer_id, time FROM appointments INNER JOIN customers USING(customer_id) INNER JOIN services USING(service_id) WHERE phone = '$CUSTOMER_PHONE' AND time = '$SERVICE_TIME'")
-
 # formating output print
-
 SERVICE_TIME_FORMATTED=$(echo $SERVICE_TIME | sed -E 's/ //')
 
 # display appointment info / send confirmation message
-echo -e "I have put you down for a "$SERVICE_NAME_FORMATTED" at "$SERVICE_TIME", "$CUSTOMER_NAME_FORMATTED"."
-
+echo -e "\nI have put you down for a "$SERVICE_NAME_FORMATTED" at "$SERVICE_TIME", "$CUSTOMER_NAME_FORMATTED".\n"
 
 }
 
