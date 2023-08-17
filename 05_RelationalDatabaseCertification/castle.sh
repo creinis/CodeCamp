0d5a83ae7e1f318a91777c89969c6a155f16377f
creinis <115181527+creinis@users.noreply.github.com>
2023-08-17 17:00:43 +0200

    nano castle

---
+++ 05_RelationalDatabaseCertification/castle.sh
@@ -0,0 +1,17 @@
+
+  GNU nano 4.8                                                         castle.sh                                                                    
+echo "
+Welcome to my castle
+"
+
+echo "
+ /\                /\ 
+/  \              /  \ 
+|  |______________|  |
+|                    |
+|   []    []    []   |
+|                    |
+|         __         |
+                                                                 [ Read 14 lines ]
+^G Get Help     ^O Write Out    ^W Where Is     ^K Cut Text     ^J Justify      ^C Cur Pos      M-U Undo        M-A Mark Text   M-] To Bracket
+^X Exit         ^R Read File    ^\ Replace      ^U Paste Text   ^T To Spell     ^_ Go To Line   M-E Redo        M-6 Copy Text   ^Q Where Was
