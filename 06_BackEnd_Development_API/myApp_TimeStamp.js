9493e2587afc7a701dd68340f82b4550766fa288
creinis <115181527+creinis@users.noreply.github.com>
2023-08-23 19:20:13 +0200

    feat: cert02 ok!

--- 06_BackEnd_Development_API/myApp_TimeStamp.js
+++ 06_BackEnd_Development_API/myApp_TimeStamp.js
@@ -18,14 +18,18 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-const invalidDate = (date) => date.toUTCString() === "Invalid Date"
+const isInvalidDate = (date) => date.toUTCString() === "Invalid Date"
 
 // your first API endpoint... 
 app.get("/api/:date", function (req, res) {
   let date = new Date(req.params.date)
 
-  if(invalidDate(date)){
+  if(isInvalidDate(date)){
     date = new Date(+req.params.date)
+  }
+
+  if(isInvalidDate(date)){
+    res.json({error: "Invalid Date"})
   }
   
   res.json({
@@ -34,7 +38,12 @@ app.get("/api/:date", function (req, res) {
   });
 });
 
-
+app.get("/api", (req, res) => {
+  res.json({
+    unix: new Date().getTime(),
+    utc: new Date().toUTCString()
+  })
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
