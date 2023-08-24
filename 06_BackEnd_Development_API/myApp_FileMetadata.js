72868ed0adfcb5cdba736938bf9ab00b9cb6637b
creinis <115181527+creinis@users.noreply.github.com>
2023-08-24 15:25:34 +0200

    feat: finish projects BackEnd and APIs Development

--- 06_BackEnd_Development_API/myApp_FileMetadata.js
+++ 06_BackEnd_Development_API/myApp_FileMetadata.js
@@ -1,1 +1,31 @@
+var express = require('express');
+var cors = require('cors');
+require('dotenv').config()
+const multer = require('multer')
+const upload = multer()
 
+
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post("/api/fileanalyse", upload.single('upfile'), (req, res) => {
+
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+  
+})
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
