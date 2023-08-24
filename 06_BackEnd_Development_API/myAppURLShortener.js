bd5a76662442fa0e126b0e3ea1e5b72d227e71a2
creinis <115181527+creinis@users.noreply.github.com>
2023-08-24 12:00:05 +0200

    fix: original url

--- 06_BackEnd_Development_API/myAppURLShortener.js
+++ 06_BackEnd_Development_API/myAppURLShortener.js
@@ -4,19 +4,19 @@ const cors = require('cors');
 const app = express();
 
 const { MongoClient } = require('mongodb');
-const urlparser = require('urls');
-const dns = require('dns');
+const dns = require('dns')
+const urlparser = require('url')
 
-const client = new MongoClient(process.env.DB_URL);
-const db = client.db("urlshortner");
-const urls = db.collection("urls");
+const client = new MongoClient(process.env.DB_URL)
+const db = client.db("urlshortner")
+const urls = db.collection("urls")
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -25,9 +25,13 @@ app.get('/', function(req, res) {
 });
 
 // Your first API endpoint
+
 app.post('/api/shorturl', function(req, res) {
+  
   console.log(req.body)
-  const dnslookup = dns.lookup(urlparser.parse(url).hostname, async (error, address) => {
+  const url = req.body.url
+  
+  const dnslookup = dns.lookup(urlparser.parse(url).hostname, async (err, address) => {
     if (!address) {
       
       res.json({error: 'invalid url'})
@@ -37,19 +41,29 @@ app.post('/api/shorturl', function(req, res) {
       /* url shortner by counting documents */
       const urlCount = await urls.countDocuments({})
       const urlDoc = {
-        url: req.body.url,
+        url,
         short_url: urlCount
         
       }
       /* Save */
       const result = await urls.insertOne(urlDoc)
-      console.log(result)
+      console.log(result);
+      res.json({ original_url: url, short_url: urlCount })
       
     }
   })
-  res.json({ greeting: 'hello API' });
 });
+
+/* When you visit /api/shorturl/<short_url>, you will be redirected to the original URL. */
+
+app.get("/api/short_url/:short_url", async (req, res) => {
+  const shorturl = req.params.short_url
+  const urlDoc = await urls.findOne({ short_url: +shorturl })
+  res.redirect(urlDoc.url)
+})
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
