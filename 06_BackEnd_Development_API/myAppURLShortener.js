eaa289220c453511ab27856aca4d287370184fb6
creinis <115181527+creinis@users.noreply.github.com>
2023-08-24 11:25:37 +0200

    feat: star tests

--- 06_BackEnd_Development_API/myAppURLShortener.js
+++ 06_BackEnd_Development_API/myAppURLShortener.js
@@ -1,11 +1,13 @@
-
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const { MongoClient } = require('mongodb');
 
-const client = new MongoClient(process.env.DB_URI);
+const { MongoClient } = require('mongodb');
+const urlparser = require('urls');
+const dns = require('dns');
+
+const client = new MongoClient(process.env.DB_URL);
 const db = client.db("urlshortner");
 const urls = db.collection("urls");
 
@@ -24,6 +26,27 @@ app.get('/', function(req, res) {
 
 // Your first API endpoint
 app.post('/api/shorturl', function(req, res) {
+  console.log(req.body)
+  const dnslookup = dns.lookup(urlparser.parse(url).hostname, async (error, address) => {
+    if (!address) {
+      
+      res.json({error: 'invalid url'})
+      
+    } else {
+
+      /* url shortner by counting documents */
+      const urlCount = await urls.countDocuments({})
+      const urlDoc = {
+        url: req.body.url,
+        short_url: urlCount
+        
+      }
+      /* Save */
+      const result = await urls.insertOne(urlDoc)
+      console.log(result)
+      
+    }
+  })
   res.json({ greeting: 'hello API' });
 });
 
