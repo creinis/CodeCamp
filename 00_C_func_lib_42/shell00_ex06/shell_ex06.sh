015b7ae6901b19e24d45f2b2c1a860fbb77dc2d4
creinis <115181527+creinis@users.noreply.github.com>
2023-08-25 11:00:14 +0200

    feat: update exs shell 00

---
+++ 00_C_func_lib_42/shell00_ex06/shell_ex06.sh
@@ -0,0 +1,19 @@
+# /* ************************************************************************** */
+# /*                                                                            */
+# /*                                                        :::      ::::::::   */
+# /*   shell 00                                           :+:      :+:    :+:   */
+# /*                                                    +:+ +:+         +:+     */
+# /*   By: creinis <creinis@student.42heilbronn.de    +#+  +:+       +#+        */
+# /*                                                +#+#+#+#+#+   +#+           */
+# /*   Created: 2022/11/27 15:30:26 by creinis           #+#    #+#             */
+# /*   Updated: 2022/12/08 10:46:45 by creinis          ###   ########.fr       */
+# /*                                                                            */
+# /* ************************************************************************** */
+
+Episode V, A NEW H0PE It is a period of civil war
+Rebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire. 
+During the battle, Rebel spies managed to steal secret plans to the Empire's ultimate weapon, the STAR DEATH, an armored space station with enough power to destroy an entire planet.
+
+
+Pursued by the Empire's sinister agents,
+Princess Mehdi races home aboard her starship, custodian of the stolen plans that can save her people and restore the dictatorship to the galaxie..
