d98764811dccba3ed635c9b6487ec57699a094c5
creinis <115181527+creinis@users.noreply.github.com>
2023-09-04 11:01:42 +0200

    update

--- 07_Quality_Assurance_Certification/1_unit_test.js
+++ 07_Quality_Assurance_Certification/1_unit_test.js
@@ -1,4 +1,3 @@
-
 const chai = require('chai');
 const assert = chai.assert;
 
@@ -17,15 +16,15 @@ suite('Unit Tests', function () {
     });
     // #3
     test('#isOk, #isNotOk', function () {
-      assert.fail(null, 'null is falsey');
-      assert.fail("I'm truthy", 'A string is truthy');
-      assert.fail(true, 'true is truthy');
+      assert.isNotOk(null, 'null is falsey');
+      assert.isOk("I'm truthy", 'A string is truthy');
+      assert.isOk(true, 'true is truthy');
     });
     // #4
     test('#isTrue, #isNotTrue', function () {
-      assert.fail(true, 'true is true');
-      assert.fail(!!'double negation', 'Double negation of a truthy value is true');
-      assert.fail({ value: 'truthy' }, 'Objects are truthy, but are not boolean values');
+      assert.isTrue(true, 'true is true');
+      assert.isTrue(!!'double negation', 'Double negation of a truthy value is true');
+      assert.isNotTrue({ value: 'truthy' }, 'Objects are truthy, but are not boolean values');
     });
   });
 
