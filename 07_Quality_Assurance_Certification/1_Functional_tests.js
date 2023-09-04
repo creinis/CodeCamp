e36018277ae517c906a90e6f41929e8abadd20ab
creinis <115181527+creinis@users.noreply.github.com>
2023-09-04 17:44:34 +0200

    functional end

--- 07_Quality_Assurance_Certification/1_Functional_tests.js
+++ 07_Quality_Assurance_Certification/1_Functional_tests.js
@@ -104,10 +104,16 @@ suite('Functional Tests with Zombie.js', function () {
     });
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
-      assert.fail();
+      browser.fill('surname', 'Vespucci').then(() => {
+        browser.pressButton('submit', () => {
+          browser.assert.success();
+          browser.assert.text('span#name', 'Amerigo');
+          browser.assert.text('span#surname', 'Vespucci');
+          browser.assert.elements('span#dates', 1);
+           done();
+        });
+      });
 
-      done();
     });
   });
 });
-
