a53ce33a52b14f29ee2870372c6e29f0d0630df1
creinis <115181527+creinis@users.noreply.github.com>
2023-09-06 16:19:02 +0200

    add files to project

---
+++ 07_Quality_Assurance_Certification/Cert01_MetricImpConverter/Project/routes/api.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const expect = require('chai').expect;
+const ConvertHandler = require('../controllers/convertHandler.js');
+
+module.exports = function (app) {
+  
+  let convertHandler = new ConvertHandler();
+  
+  app.rout('/api/convert')
+  .get(function (req, res) {
+      var input = req.query.input;
+      var initNum = convertHandler.getNum(input);
+      var initUnit = convertHandler.getUnit(input);
+      var returnNum = convertHandler.convert(initNum, initUnit);
+      var returnUnit = convertHandler.getReturnUnit(initUnit);
+      var toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+    
+      let responseObject = {}
+      responseObject['initNum'] = initNum
+      responseObject['initUnit'] = initUnit
+      responseObject['returnNum'] = returnNum
+      responseObject['returnUnit'] = returnUnit
+      responseObject['string'] = toString
+    
+      res.json(responseObject)
+    
+       });
+
+};
