72180b7350d6fd786bdb89eeac968d2703dfdc96
creinis <115181527+creinis@users.noreply.github.com>
2023-09-08 15:01:15 +0200

    add post routes

---
+++ 07_Quality_Assurance_Certification/Cert02_Issue_Tracker/models.js
@@ -0,0 +1,24 @@
+
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const IssueSchema = new Schema({
+  issue_title: { type: String, required: true },
+  issue_text: { type: String, required: true },
+  created_on: Date,
+  updated_on: Date,
+  created_by: { type: String, required: true },
+  assigned_to: String,
+  open: Boolean,
+  status_text: String,
+});
+const Issue = mongoose.model("Issue", IssueSchema);
+
+const ProjectSchema = new Schema({
+  name: { type: String, required: true },
+  issues: [IssueSchema],
+});
+const Project = mongoose.model("Project", ProjectSchema);
+
+exports.Issue = Issue;
+exports.Project = Project;
--- 07_Quality_Assurance_Certification/Cert02_Issue_Tracker/routes/api.js
+++ 07_Quality_Assurance_Certification/Cert02_Issue_Tracker/routes/api.js
@@ -1,5 +1,11 @@
 'use strict';
 
+const mongoose = require("mongoose");
+const IssueModel = require("../models").Issue;
+const ProjectModel = require("../models").Project;
+const ObjectId = mongoose.Types.ObjectId;
+
+
 module.exports = function (app) {
 
   app.route('/api/issues/:project')
@@ -9,10 +15,53 @@ module.exports = function (app) {
       
     })
     
-    .post(function (req, res){
+    .post(function (req, res) {
       let project = req.params.project;
-      
+      const {
+        issue_title,
+        issue_text,
+        created_by,
+        assigned_to,
+        status_text,
+      } = req.body;
+      if (!issue_title || !issue_text || !created_by) {
+        res.json({ error: 'required field(s) missing' });
+        return;
+      }
+      const newIssue = new IssueModel({
+        issue_title: issue_title || "",
+        issue_text: issue_text || "",
+        created_on: new Date(),
+        updated_on: new Date(),
+        created_by: created_by || "",
+        assigned_to: assigned_to || "",
+        open: true,
+        status_text: status_text || "",
+      });
+      ProjectModel.findOne({ name: project }, (err, projectdata) => {
+        if (!projectdata) {
+          const newProject = new ProjectModel({ name: project });
+          newProject.issues.push(newIssue);
+          newProject.save((err, data) => {
+            if (err || !data) {
+              res.send("There was an error saving in post");
+            } else {
+              res.json(newIssue);
+            }
+          });
+        } else {
+          projectdata.issues.push(newIssue);
+          projectdata.save((err, data) => {
+            if (err || !data) {
+              res.send("There was an error saving in post");
+            } else {
+              res.json(newIssue);
+            }
+          });
+        }
+      });
     })
+
     
     .put(function (req, res){
       let project = req.params.project;
