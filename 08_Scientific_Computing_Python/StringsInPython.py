75fa89a203afe649688a9decfd9a45ca1e174acc
creinis <115181527+creinis@users.noreply.github.com>
2023-12-07 17:37:12 +0100

    iterations

---
+++ 08_Scientific_Computing_Python/StringsInPython.py
@@ -0,0 +1,9 @@
+fruit = "banana"
+index = 0
+while index < len(fruit):
+    letter = fruit[index]
+    print(index, letter)
+    index = index + 1
+for n in "banana":
+    print(n)
+print(len(fruit))
