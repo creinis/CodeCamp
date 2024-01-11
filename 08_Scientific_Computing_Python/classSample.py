e65666ce4d3d8cdf701cd7d980525279cb86b7e1
creinis <115181527+creinis@users.noreply.github.com>
2024-01-11 18:34:57 +0100

    Object Lifecycle

---
+++ 08_Scientific_Computing_Python/classSample.py
@@ -0,0 +1,35 @@
+'Class Sample'
+
+print('Class Sample Exercise')
+class PartyAnimal:
+    x = 0
+    def party(self):
+        self.x = self.x + 2
+        print(self.x)
+
+an = PartyAnimal()
+an.party()
+an.party()
+print('\n')
+
+'Object Lifecycle'
+
+print('Object Lifecycle Exercise')
+class PartyAnimal:
+    x = 0
+    name = ''
+    def __init__(self, nam):
+        self.name = nam
+        print(self.name,'constructed')
+    def party(self):
+        self.x = self.x + 1
+        print(self.name,'party count',self.x)
+
+q = PartyAnimal('Quincy')
+m = PartyAnimal('Miya')
+
+q.party()
+m.party()
+q.party()
+print('\n')
+
---
+++ 08_Scientific_Computing_Python/geolocation.py
@@ -0,0 +1,30 @@
+import urllib.request, urllib.parse, urllib.error
+import json
+
+serviceurl = 'https://maps.googleapis.com/maps/api/geocode/json?'
+
+while True:
+    address = input('Enter location: ')
+    if len(address) < 1: break
+    
+    url = serviceurl + urllib.parse.urlencode({'address': address})
+    
+    print('Retrieving', url)
+    uh = urllib.request.urlopen(url)
+    data = uh.read().decode()
+    print('Retrieved', len(data), 'characters')
+    
+    try:
+        js = json.loads(data)
+    except:
+        js = None
+    
+    if not js or 'status' not in js or js['status'] != 'OK' :
+        print('=== Failure to Retrieve ===')
+        print(data)
+        continue
+    lat = js["results"][0]["geometry"]["location"]["lat"]
+    lng = js["results"][0]["geometry"]["location"]["lng"]
+    print('lat', lat, 'lng', lng)
+    location = js['results'][0]['formatted_address']
+    print(location)
