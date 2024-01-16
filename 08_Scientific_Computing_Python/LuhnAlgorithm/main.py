5bc4e5606b5885d1acff206e273e15ba14fb4f61
creinis <115181527+creinis@users.noreply.github.com>
2024-01-16 18:25:42 +0100

    finish Luhn Algorithm

--- 08_Scientific_Computing_Python/LuhnAlgorithm/main.py
+++ 08_Scientific_Computing_Python/LuhnAlgorithm/main.py
@@ -234,7 +234,129 @@ def verify_card_number(card_number):
     for digit in even_digits:
         number = int(digit) * 2
 
-# 
+# Step 27
+
+# To prevent the multiplication of one digit from being greater than 9, 
+# within the even digit loop, add an if statement that checks if number is greater than or equal to 10. 
+# If it is, print number.
+
+    sum_of_even_digits = 0
+    even_digits = card_number_reversed[1::2]
+    for digit in even_digits:
+        number = int(digit) * 2
+        if number >= 10:
+            print(number)
+
+# Step 28
+
+# Part of the algorithm is to double every second digit, starting from the right. 
+# If the result of doubling the number is greater than or equal to 10, add the two digits together. 
+# For example, if the digit is 6, double it to get 12. Add 1 and 2 together to get 3. 
+# You can do this by using integer division to get the first digit and the modulus operator (%) to get the second digit:
+
+my_number = 12
+first_digit = my_number // 10
+second_digit = my_number % 10
+
+# Integer division results in the quotient of the division, rounded down to the nearest integer.
+# Within the if statement, assign number the result of number // 10 (integer division) 
+# plus the modulus of number and 10.
+
+sum_of_even_digits = 0
+even_digits = card_number_reversed[1::2]
+for digit in even_digits:
+    number = int(digit) * 2
+    if number >= 10:
+        number = (number // 10) + (number % 10)
+        print(number)
+
+# Step 30
+
+# Outside of the if statement, add number to sum_of_even_digits. Also, remove the print call.
+
+sum_of_even_digits = 0
+even_digits = card_number_reversed[1::2]
+for digit in even_digits:
+    number = int(digit) * 2
+    if number >= 10:
+        number = (number // 10) + (number % 10)
+    sum_of_even_digits += number
+
+# Step 31
+
+# Below the second for loop of the verify_card_number function, create a variable named total, 
+# and assign it the value of the sum of the odd and even digits. 
+# Print total to the console.
+
+sum_of_even_digits = 0
+even_digits = card_number_reversed[1::2]
+for digit in even_digits:
+    number = int(digit) * 2
+    if number >= 10:
+        number = (number // 10) + (number % 10)
+    sum_of_even_digits += number
+total = sum_of_odd_digits + sum_of_even_digits
+print(total)
+
+# Step 32
+
+# Return the result of comparing 0 to total modulo 10.
+
+total = sum_of_odd_digits + sum_of_even_digits
+print(total)
+# return 0 == total % 10
+
+# Step 33
+
+#Adjust the verify_card_number call such that if it returns True, print VALID! to the console. 
+# Otherwise, print INVALID!.
+
+def main():
+    card_number = '4111-1111-4555-1142'
+    card_translation = str.maketrans({'-': '', ' ': ''})
+    translated_card_number = card_number.translate(card_translation)
+
+    if verify_card_number(translated_card_number):
+        print("VALID!")
+    else:
+        print("INVALID!")
+
+# Step 35
+
+# Well done on completing this project.
+
+# As a final step, remove the print call from the verify_card_number function, 
+# and change the card_number back to something valid.
+
+def verify_card_number(card_number):
+    sum_of_odd_digits = 0
+    card_number_reversed = card_number[::-1]
+    odd_digits = card_number_reversed[::2]
+
+    for digit in odd_digits:
+        sum_of_odd_digits += int(digit)
+
+    sum_of_even_digits = 0
+    even_digits = card_number_reversed[1::2]
+    for digit in even_digits:
+        number = int(digit) * 2
+        if number >= 10:
+            number = (number // 10) + (number % 10)
+        sum_of_even_digits += number
+    total = sum_of_odd_digits + sum_of_even_digits
+    return total % 10 == 0
+
+def main():
+    card_number = '4111-1111-4555-1142'
+    card_translation = str.maketrans({'-': '', ' ': ''})
+    translated_card_number = card_number.translate(card_translation)
+
+    if verify_card_number(translated_card_number):
+        print('VALID!')
+    else:
+        print('INVALID!')
+
+main()
 
 
 
