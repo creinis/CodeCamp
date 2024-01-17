b9e25dd96722506887b5ba000ad9fedc5ef1b087
creinis <115181527+creinis@users.noreply.github.com>
2024-01-17 09:06:40 +0100

    Lambda

---
+++ 08_Scientific_Computing_Python/ExpenseTracker/main.py
@@ -0,0 +1,55 @@
+# Lambda
+
+# Step 1
+
+# In this project, you're going to build a simple, yet functional expense tracker in Python.
+
+# Start by defining a function named add_expense that takes three parameters: 
+# expenses, amount and category. Use the pass keyword to fill the function body.
+
+def add_expense(expenses, amount, category):
+    pass
+
+# Step 2
+
+# A list in Python is a built-in data type that allows you to store many items in a single data structure. 
+# In Python, you create a list by putting items inside square brackets [], 
+# with each item separated from the following one by a comma.
+
+numbers = [1, 2, 3, 4]
+
+# Create an empty list named expenses. You will use it to store each of your expenses.
+
+expenses = []
+
+# Step 3
+
+# The expenses parameter of your add_expense function will be a list of expenses. 
+# You want to be able to add items at the end of your list. For that you can use the .append() list method:
+
+my_list = [2, 4, 7]
+my_list.append(3)
+
+#In the example above, after appending 3, my_list would be [2, 4, 7, 3]. 
+# Replace pass with a call to the .append() method on the expenses list. 
+# Don't pass any arguments to .append() for now.
+
+def add_expense(expenses, amount, category):
+    expenses.append()
+
+# Step 4
+
+# A dictionary is another built-in data type in Python. 
+# A dictionary is a collection of data in the form of key-value pairs. 
+# Dictionaries are defined with curly braces {} and they contain key-value pairs separated by commas. 
+# Each key is followed by a colon : and the value:
+
+{"amount": 50.0, "category": "Food"}
+
+# In the example above, amount and category are keys, and 50.0 and Food are their the corresponding values.
+
+# Create a dictionary with a key amount and value of the amount parameter and pass your new dictionary to the .append() call.
+
+
+
+
