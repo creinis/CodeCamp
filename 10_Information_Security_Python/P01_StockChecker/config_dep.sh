721bbdbad26b7530c8bcadcae67f8cb9c6520339
creinis <115181527+creinis@users.noreply.github.com>
2024-02-05 09:05:57 +0100

    add scripts/tests ins pck json

---
+++ 10_Information_Security_Python/P01_StockChecker/config_dep.sh
@@ -0,0 +1,14 @@
+#!bin/bash/shell
+
+print('change this file to executable permission chmod +x config_dep.sh')
+#!chmod +x config_dep.sh
+
+print('install dotenv')
+npm i dotenv
+print('install request for external api')
+npm i request
+print('install mongodb dependency to connect to db')
+npm i mongodb
+
+npm i zombie
+
