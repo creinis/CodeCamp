04ddf5b225309c72b36f70283606680488f771d7
creinis <115181527+creinis@users.noreply.github.com>
2024-02-12 18:55:17 +0100

    problems..

--- 10_Information_Security_Python/P01_StockChecker/db-connection.js
+++ 10_Information_Security_Python/P01_StockChecker/db-connection.js
@@ -1,9 +1,15 @@
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
-const db = mongoose.connect(process.env.DB, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
+mongoose.Promise = global.Promise;
+dotenv.config({ path: ".env" });
+const db = mongoose.connect(process.env.DB, {});
+
+// Check connection
+const connection = mongoose.connection;
+connection.on("error", console.error.bind(console, "DB connection error:"));
+connection.once("open", () => {
+  console.log("DB connection established successfully");
 });
 
 module.exports = db;
