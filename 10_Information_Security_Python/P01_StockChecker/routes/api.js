6e8669ea181899d9ce6c031a66a7eda357ec3450
creinis <115181527+creinis@users.noreply.github.com>
2024-02-15 10:16:43 +0100

    update

--- 10_Information_Security_Python/P01_StockChecker/routes/api.js
+++ 10_Information_Security_Python/P01_StockChecker/routes/api.js
@@ -1,53 +1,44 @@
-'use strict';
+// "use strict" não foi alterado, mantendo o mesmo comportamento
 
-const mongoose = require('mongoose');
-const objectId = mongoose.Types.ObjectId;
-const mongodb = require('mongodb');
-const request = require('request-promise-native');
+const mongoose = require("mongoose");
+const mongodb = require("mongodb");
+const request = require("request-promise-native");
 
-
-
-// db schema model
+// Definição do Esquema do Banco de Dados
 let stockSchema = new mongoose.Schema({
-  code: String,
-  likes: {type: [Number], default: []}
-})
+  name: { type: String, required: true }, 
+  likes: { type: [Number], default: [] },
+});
 
-let Stock = mongoose.model('stock', stockSchema)
+let Stock = mongoose.model("stock", stockSchema);
 
-// save stock
-function saveStock(code, like, ip) {
-  return Stock.findOne({ code: code})
-  .then(stock => {
-    if(!stock) {
-      let newStock = new Stock({ code: code, likes: like ? [ip]:[]})
-      return newStock.save()
+// Função para salvar ações
+function saveStock(name, like, ip) {
+  return Stock.findOne({ name: name }).then((stock) => {
+    if (!stock) {
+      let newStock = new Stock({ name: name, likes: like ? [ip] : [] });  
+      return newStock.save();
     } else {
-      if(like && stock.likes.indexOf(ip) === -1) {
-        stock.likes.push(ip)
+      if (like && stock.likes.indexOf(ip) === -1) {
+        stock.likes.push(ip);
       }
-      return stock.save()
+      return stock.save();
     }
-  })
+  });
 }
 
-// Parse Data
-function parseData(data){
-
-  console.log(data);
-
+// Função para analisar os dados recebidos
+function parseData(data) {
   let i = 0;
   let stockData = [];
   let likes = [];
   while (i < data.length) {
-    let stock = { stock: data[i].code, price: JSON.parse(data[i+1]).close};
-    console.log(typeof(data[i+1]));
-    console.log(JSON.parse(data[i+1]))
+    let stock = { stock: data[i].name, price: JSON.parse(data[i + 1]).close };
     likes.push(data[i].likes.length);
     stockData.push(stock);
     i += 2;
   }
-  if(likes.length > 1) {
+  if (likes.length > 1) {
     stockData[0].rel_likes = likes[0] - likes[1];
     stockData[1].rel_likes = likes[1] - likes[0];
   } else {
@@ -59,33 +50,28 @@ function parseData(data){
 }
 
 module.exports = function (app) {
-  
-  app.get('/api/testing', (req, res) => {
-    res.json({ IP: req.ip })
-  })
+  app.get("/api/testing", (req, res) => {
+    res.json({ IP: req.ip });
+  });
 
-  app.route('/api/stock-prices')
-    .get(function (req, res){
-    
-      let code = req.query.stock || '';
-      if (!Array.isArray(code)) {
-        code = [code];
-      }
-      let promises = [];
-      code.forEach(code => {
-        promises.push(saveStock(code.toUpperCase(), req.query.like, req.ip));
-        let url = `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${code.toUpperCase()}/quote`
-        promises.push(request(url))
-      });
-      Promise.all(promises)
-      .then(data => {
+  app.route("/api/stock-prices").get(function (req, res) {
+    let name = req.query.stock || "";
+    if (!Array.isArray(name)) {
+      name = [name];
+    }
+    let promises = [];
+    name.forEach((name) => {
+      promises.push(saveStock(name.toUpperCase(), req.query.like, req.ip));
+      let url = `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${name.toUpperCase()}/quote`;
+      promises.push(request(url));
+    });
+    Promise.all(promises)
+      .then((data) => {
         let stockData = parseData(data);
-        res.json({ stockData })
+        res.json({ stockData });
       })
-      .catch(err => {
-        console.log(err);
+      .catch((err) => {
         res.send(err);
-      })
-
       });
-    };
+  });
+};
