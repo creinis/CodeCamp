e3784af0f63c31cb171ee1a0b5378cfbff1123ca
creinis <115181527+creinis@users.noreply.github.com>
2024-02-16 17:56:48 +0100

    Anonymous Message Board - start

---
+++ 10_Information_Security_Python/P02_AnonymousMessageBoard/server.js
@@ -0,0 +1,88 @@
+'use strict';
+require('dotenv').config();
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const cors        = require('cors');
+
+const apiRoutes         = require('./routes/api.js');
+const fccTestingRoutes  = require('./routes/fcctesting.js');
+const runner            = require('./test-runner');
+
+let helmet = require("helmet");
+const app = express();
+
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.use(cors({origin: '*'})); //For FCC testing purposes only
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Helmet
+//Only allow your site to be loaded in an iFrame on your own pages.
+app.use(
+  helmet({
+    xFrameOptions: { action: "sameorigin" },
+  })
+);
+//Do not allow DNS prefetching.
+app.use(
+  helmet({
+    xDnsPrefetchControl: { allow: false },
+  })
+);
+//Only allow your site to send the referrer for your own pages.
+app.use(
+  helmet({
+    referrerPolicy: {
+      policy: "same-origin",
+    },
+  })
+);
+
+//Sample front-end
+app.route('/b/:board/')
+  .get(function (req, res) {
+    res.sendFile(process.cwd() + '/views/board.html');
+  });
+app.route('/b/:board/:threadid')
+  .get(function (req, res) {
+    res.sendFile(process.cwd() + '/views/thread.html');
+  });
+
+//Index page (static HTML)
+app.route('/')
+  .get(function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+//For FCC testing purposes
+fccTestingRoutes(app);
+
+//Routing for API 
+apiRoutes(app);
+
+//404 Not Found Middleware
+app.use(function(req, res, next) {
+  res.status(404)
+    .type('text')
+    .send('Not Found');
+});
+
+//Start our server and tests!
+const listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+  if(process.env.NODE_ENV==='test') {
+    console.log('Running Tests...');
+    setTimeout(function () {
+      try {
+        runner.run();
+      } catch(e) {
+        console.log('Tests are not valid:');
+        console.error(e);
+      }
+    }, 1500);
+  }
+});
+
+module.exports = app; //for testing
