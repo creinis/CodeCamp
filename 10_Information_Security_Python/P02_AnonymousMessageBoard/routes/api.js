56f900940da411a86aec526b0fad79d305787b4a
creinis <115181527+creinis@users.noreply.github.com>
2024-02-20 14:38:17 +0100

    still with problems

--- 10_Information_Security_Python/P02_AnonymousMessageBoard/routes/api.js
+++ 10_Information_Security_Python/P02_AnonymousMessageBoard/routes/api.js
@@ -28,8 +28,6 @@ module.exports = function (app) {
   // THREADS
   app.route('/api/threads/:board')
     .post((req, res) => {
-      // POST: You can send a POST request to /api/replies/{board} with form data including text, 
-      // delete_password, & thread_id. This will update the bumped_on date to the comment's date. 
       let postThread = req.body
       let newThread = new Thread({
         text: postThread.text,
@@ -51,134 +49,129 @@ module.exports = function (app) {
         .then((d) => res.json(d))
         .catch(err => console.log(err))
     })
-    .delete((req, res) => {
-      // DELETE: You can send a DELETE request to /api/threads/{board} and pass along the thread_id & delete_password 
-      // to delete the thread. Returned will be the string incorrect password or success.
-      let deleteThread = req.body
-      console.log("Deleting Thread: ", deleteThread.thread_id); 
-      Thread.findOneAndDelete({
-        _id: mongodb.ObjectId(deleteThread.thread_id),
-        delete_password: deleteThread.delete_password,
-      },
-        function(err, data) {
-          if(err) {
-            console.log("Error deleting thread: ", err);
-            return res.status(400).json('cannot delete Thread data')
-          } else if (data) {
-            console.log("Seccess deleting thread")
-            console.log(typeof(res.json));
-            return res.json('success')
-          } else {
-            console.log("Incorrect password while deleting thread")
-            console.log(typeof(res.json));
-            return res.json('incorrect password')
-          }
+    .delete(async (req, res) => {
+      try {
+        const { thread_id, delete_password } = req.body;
+        console.log(`Deleting thread with id ${thread_id} and password ${delete_password}`);
+        const result = await Thread.findOneAndDelete({ _id: thread_id, delete_password: delete_password });
+        console.log('findOneAndDelete result:', result);
+        if (result) {
+          res.send('success');
+        } else {
+          console.log('Incorrect password while deleting thread');
+          res.status(401).send('incorrect password');
         }
-      )
+      } catch (error) {
+        console.error('Error in DELETE /api/threads/:board:', error);
+        res.status(500).send('internal server error');
+      }
     })
-    .put((req, res) => {
-      // PUT: You can send a PUT request to /api/threads/{board} and pass along the thread_id. 
-      // Returned will be the string reported. The reported value of the thread_id will be changed to true.
-      let putThread = req.body
-      Thread.findOneAndUpdate({_id: putThread.thread_id}, {$set: {reported: true}})
-        .then(() => res.json('success'))
-        .catch(() => { return res.status(400).json('cannot update threat')})
+    .put(async (req, res) => {
+      try {
+        const { thread_id } = req.body;
+        await Thread.findByIdAndUpdate(thread_id, { reported: true });
+        res.send('reported');
+      } catch (error) {
+        console.error(error);
+        res.status(500).send('internal server error');
+      }
     })
     
   // REPLIES
   app.route('/api/replies/:board')
-    .post((req, res) => {
-      // POST: You can send a POST request to /api/replies/{board} with form data including text, delete_password, 
-      // & thread_id. This will update the bumped_on date to the comment's date.
-      let postThread = req.body
-      let postParams = req.params.board
-      let newReplies = {
-        _id: new mongodb.ObjectId(),
-        text: postThread.text,
-        created_on: new Date(),
-        delete_password: postThread.delete_password,
-        reported: false,
+  .post((req, res) => {
+    let postThread = req.body
+    let postParams = req.params.board
+    let newReplies = {
+      _id: new mongodb.ObjectId(),
+      text: postThread.text,
+      created_on: new Date(),
+      delete_password: postThread.delete_password,
+      reported: false,
+    }
+    Thread.findOneAndUpdate({_id: postThread.thread_id}, {$inc: {replycount: 1}, $push: {
+      replies: {
+        $each: [newReplies],
+        $sort: {created_on: -1}
       }
-      Thread.findOneAndUpdate({_id: postThread.thread_id}, {$inc: {replycount: 1}, $push: {
-        replies: {
-          $each: [newReplies],
-          $sort: {created_on: -1}
-        }
-      }})
-        .then(() => res.redirect('/b/' + postParams + '/' + postThread.thread_id))
-        .catch(() => console.log(err))
+    }})
+      .then(() => res.redirect('/b/' + postParams + '/' + postThread.thread_id))
+      .catch(() => console.log(err))
 
-    })
-    .get((req, res) => {
-      // GET: You can send a GET request to /api/replies/{board}?thread_id={thread_id}. 
-      // Returned will be the entire thread with all its replies, also excluding the same fields from the 
-      // client as in the THREADS routes suit.
-      let postThread = req.query.thread_id;
-      Thread.findOne(
-        {_id: postThread},
-        {
-          delete_password: 0,
-          reported: 0,
-          __v: 0,
-          "replies.delete_password": 0,
-          "replies.reported": 0
-        }
-      )
-        .then((d) => res.json(d))
-        .catch(err => console.log(err))
-    })
-    .delete((req, res) => {
-      // DELETE: You can send a DELETE request to /api/replies/{board} and pass along the thread_id, reply_id, 
-      // & delete_password. Returned will be the string incorrect password or success. On success, the text of 
-      // the reply_id will be changed to [deleted].
-      let deleteThread = req.body
-      console.log("Deleting Reply: ", deleteThread.reply_id);
-      Thread.findOneAndUpdate(
-        {
-          _id: mongodb.ObjectId(deleteThread.thread_id),
-          'replies._id': mongodb.ObjectId(deleteThread.reply_id),
-          'replies.delete_password': deleteThread.delete_password
-        },
-        {
-          $set: {"replies.$.text": '[deleted]'}
-        }
-      )
-        .then((d) => {
-          if(d){
-            console.log("Success delete reply")
-            return res.json('success')
+  })
+  .get((req, res) => {
+    // GET: You can send a GET request to /api/replies/{board}?thread_id={thread_id}. 
+    // Returned will be the entire thread with all its replies, also excluding the same fields from the 
+    // client as in the THREADS routes suit.
+    let postThread = req.query.thread_id;
+    Thread.findOne(
+      {_id: postThread},
+      {
+        delete_password: 0,
+        reported: 0,
+        __v: 0,
+        'replies.delete_password': 0,
+        'replies.reported': 0
+      }
+    )
+      .then((d) => res.json(d))
+      .catch(err => console.log(err))
+  })
+  
+  .put((req, res) => {
+    // PUT: You can send a PUT request to /api/replies/{board} and pass along the thread_id & reply_id. 
+    // Returned will be the string reported. The reported value of the reply_id will be changed to true.
+    let putThread = req.body
+    console.log('Reporting Reply: ', putThread.reply_id);
+    Thread.findOneAndUpdate(
+      {
+        _id: putThread.thread_id,
+        'replies._id': mongodb.ObjectId(putThread.reply_id)
+      },
+      {
+        $set: {'replies.$.reported': true}
+      }
+    )
+      .then(() => {
+        console.log('Success - Reported Reply');
+        res.json('reported')
+      })
+      .catch(() => {
+        console.log('Error Reporting Reply: ', err)
+        return res.status(400).json('cannot report the reply thread');
+      })
+  })
+  .delete((req, res) => {
+    let deleteThread = req.body;
+    console.log('Deleting Reply: ', deleteThread.reply_id);
+    Thread.findOne(
+      {
+        _id: mongodb.ObjectId(deleteThread.thread_id),
+        'replies._id': mongodb.ObjectId(deleteThread.reply_id)
+      }
+    )
+      .then((thread) => {
+        if(thread){
+          let reply = thread.replies.id(deleteThread.reply_id);
+          if(reply.delete_password === deleteThread.delete_password){
+            reply.text = '[deleted]';
+            thread.save().then(() => {
+              console.log('Success delete reply');
+              res.send('success');
+            });
           } else {
-            console.log("Incorrect password while delete reply")
-            return res.json('incorrect password')
+            console.log('Incorrect password while delete reply');
+            res.send('incorrect password');
           }
-        })
-        .catch((err) => {
-          console.log("Error deleting Reply: ", err)
-          res.status(400),json('cannot delete')
-        })
-    })
-    .put((req, res) => {
-      // PUT: You can send a PUT request to /api/replies/{board} and pass along the thread_id & reply_id. 
-      // Returned will be the string reported. The reported value of the reply_id will be changed to true.
-      let putThread = req.body
-      console.log("Reporting Reply: ", putThread.reply_id);
-      Thread.findOneAndUpdate(
-        {
-          _id: putThread.thread_id,
-          'replies._id': mongodb.ObjectId(putThread.reply_id)
-        },
-        {
-          $set: {'replies.$.reported': true}
+        } else {
+          console.log('Thread or reply not found');
+          res.send('thread or reply not found');
         }
-      )
-        .then(() => {
-          console.log("Success - Reported Reply");
-          res.json('reported')
-        })
-        .catch(() => {
-          console.log("Error Reporting Reply: ", err)
-          return res.status(400).json('cannot report the reply thread');
-        })
-    })
+      })
+      .catch((err) => {
+        console.log('Error deleting Reply: ', err);
+        res.status(400).json('cannot delete');
+      });
+  })
 
 };
--- 10_Information_Security_Python/P02_AnonymousMessageBoard/tests/2_functional-tests.js
+++ 10_Information_Security_Python/P02_AnonymousMessageBoard/tests/2_functional-tests.js
@@ -5,49 +5,152 @@ const server = require('../server');
 
 chai.use(chaiHttp);
 
-
-
-
-// Test03 - Deleting a thread with the incorrect password: DELETE request to /api/threads/{board} with an invalid delete_password
-// Test04 - Deleting a thread with the correct password: DELETE request to /api/threads/{board} with a valid delete_password
-// Test05 - Reporting a thread: PUT request to /api/threads/{board}
-// Test06 - Creating a new reply: POST request to /api/replies/{board}
-// Test07 - Viewing a single thread with all replies: GET request to /api/replies/{board}
-// Test08 - Deleting a reply with the incorrect password: DELETE request to /api/replies/{board} with an invalid delete_password
-// Test09 - Deleting a reply with the correct password: DELETE request to /api/replies/{board} with a valid delete_password
-// Test10 - Reporting a reply: PUT request to /api/replies/{board}
-
-
 suite('Functional Tests', function() {
+
+    let testThreadId;
+    let testReplyId;
 
     // Test01 - Creating a new thread: POST request to /api/threads/{board}
     test("Test01 - Creating a new thread: POST request to /api/threads/{board}", done => {
         chai.request(server)
-        .post("/api/threads/test")
+        .post(`/api/threads/test-board`)
         .send({
-            board: "test",
-            text: "textExemple",
-            delete_password: "delete_me"
+            board: 'test-board',
+            text: 'textExemple Test01',
+            delete_password: 'delete_me'
         })
         .end((err, res) => {
-            assert.equal(res.status, 200)
+            assert.equal(res.status, 200);
+            assert.exists(res.body, '_id');
+            testThreadId = res.body._id;
         })
-        done()
+        done();
     })
     // Test02 - Viewing the 10 most recent threads with 3 replies each: GET request to /api/threads/{board}
     test("Test02 - Viewing the 10 most recent threads with 3 replies each: GET request to /api/threads/{board}", done => {
         chai.request(server)
-        .get("/api/threads/test")
+        .get(`/api/threads/test-board`)
+        .end((err, res) => {
+            assert.equal(res.status, 200);
+            assert.isArray(res.body)
+            assert.isAtMost(res.body.length, 10)
+            res.body.forEach(thread => {
+                assert.isArray(thread.replies);
+                assert.isAtMost(thread.replies.length, 3);
+            });
+        })
+        done();
+    })
+    // Test03 - Deleting a thread with the incorrect password: DELETE request to /api/threads/{board} with an invalid delete_password
+    test("Test03 - Deleting a thread with the incorrect password: DELETE request to /api/threads/{board} with an invalid delete_password", done => {
+        chai.request(server)
+        .delete(`/api/threads/test-board/${testThreadId}`)
+        .send({
+            delete_password: 'incorrect password',
+            thread_id: testThreadId
+        })
         .end((err, res) => {
             assert.equal(res.status, 200)
-            assert.isArray(res.body)
-            assert.isBelow(res.body.length, 11)
-            assert.isBelow(res.body[0].replies.length, 4)
-            testThreadId0 = String(res.body[0]._id)
-            testThreadId1 = String(res.body[1]._id)
+            assert.equal(res.text, 'incorrect password')
         })
-        done()
+        done();
     })
-
-
+    // Test04 - Deleting a thread with the correct password: DELETE request to /api/threads/{board} with a valid delete_password
+    test("Test04 - Deleting a thread with the correct password: DELETE request to /api/threads/{board} with a valid delete_password", done => {
+        chai.request(server)
+        .delete(`/api/threads/test-board/${testThreadId}`)
+        .send({
+            delete_password: 'delete_me',
+        })
+        .end((err, res) => {
+            assert.equal(res.status, 200)
+            assert.equal(res.text, 'success')
+        })
+        done();
+    })
+    // Test05 - Reporting a thread: PUT request to /api/threads/{board}
+    test("Test05 - Reporting a thread: PUT request to /api/threads/{board}", done => {
+        chai.request(server)
+        .put(`/api/threads/test-board/${testThreadId}`)
+        .end((err, res) => {
+            assert.equal(res.status, 200)
+            assert.equal(res.text, 'reported')
+        })
+        done();
+    })
+    // Test06 - Creating a new reply: POST request to /api/replies/{board}
+    test("Test06 - Creating a new reply: POST request to /api/replies/{board}", done => {
+        chai.request(server)
+        .post(`/api/replies/test-board`)
+        .send({
+            thread_id: testThreadId, 
+            text: 'Test 06 Reply',
+            delete_password: 'delete_me'
+        })
+        .end((err, res) => {
+            assert.equal(res.status, 200);
+            assert.exists(res.body, '_id');
+            testReplyId = res.body._id
+        });
+        done();
+    })
+    // Test07 - Viewing a single thread with all replies: GET request to /api/replies/{board}
+    test("Test07 - Viewing a single thread with all replies: GET request to /api/replies/{board}", done => {
+        chai.request(server)
+        .get(`/api/replies/test-board`)
+        .query({
+            thread_id: testThreadId
+        })
+        .end((err, res) => {
+            assert.equal(res.status, 200);
+            //assert.exists(res.body, '_id');
+            //assert.exists(res.body, 'delete_password');
+            //assert.isArray(res.body.replies);
+        });
+        done();
+    })
+    // Test08 - Deleting a reply with the incorrect password: DELETE request to /api/replies/{board} with an invalid delete_password
+    test("Test08 - Deleting a reply with the incorrect password: DELETE request to /api/replies/{board} with an invalid delete_password", done => {
+        chai.request(server)
+        .delete(`/api/replies/test-board`)
+        .send({
+            thread_id: testThreadId,
+            reply_id: testReplyId,
+            delete_password: 'incorrect password'
+        })
+        .end((err, res) => {
+            assert.equal(res.status, 200);
+            assert.equal(res.text, 'incorrect password')
+        });
+        done();
+    })
+    // Test09 - Deleting a reply with the correct password: DELETE request to /api/replies/{board} with a valid delete_password
+    test("Test09 - Deleting a reply with the correct password: DELETE request to /api/replies/{board} with a valid delete_password", done => {
+        chai.request(server)
+        .delete(`/api/replies/test-board/${testThreadId}`)
+        .send({
+            delete_password: 'delete_me',
+            thread_id: testThreadId,
+            reply_id: testReplyId,
+        })
+        .end((err, res) => {
+            assert.equal(res.status, 200);
+            assert.equal(res.text, 'success')
+        });
+        done();
+    })
+    // Test10 - Reporting a reply: PUT request to /api/replies/{board}
+    test("Test10 - Reporting a reply: PUT request to /api/replies/{board}", done => {
+        chai.request(server)
+        .put(`/api/replies/test-board/${testThreadId}`)
+        .send({
+            thread_id: testThreadId,
+            reply_id: testReplyId
+        })
+        .end((err, res) => {
+            assert.equal(res.status, 200);
+            assert.equal(res.text, 'reported');
+        });
+        done();
+    })
 });
