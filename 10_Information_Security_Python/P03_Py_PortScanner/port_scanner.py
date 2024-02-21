248f9978529e27875fff2da784c5e93d31231a32
creinis <115181527+creinis@users.noreply.github.com>
2024-02-21 17:25:36 +0100

    just one more test to pass!

--- 10_Information_Security_Python/P03_Py_PortScanner/port_scanner.py
+++ 10_Information_Security_Python/P03_Py_PortScanner/port_scanner.py
@@ -3,38 +3,34 @@ import re
 from common_ports import ports_and_services
 
 def get_open_ports(target, port_range, verbose=False):
-    
-    ip = ""
-
+    open_ports = []
     try:
         ip = socket.gethostbyname(target)
     except socket.gaierror:
         if re.search('[a-zA-Z]', target):
             return "Error: Invalid hostname"
-        else:
-            return "Error: Invalid IP address"
+        return "Error: Invalid IP address"
     except socket.error:
         return "Error: Invalid IP address"
-    
-    open_ports = []
-    
+
     for port in range(port_range[0], port_range[1] + 1):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(1)
-        result = sock.connect_ex((ip, port))
-        if result == 0:
-            open_ports.append(port)
-        sock.close()
-
-    hostname = None
-
-    try:
-        hostname = socket.gethostbyaddr(ip)[0]
-    except socket.error:
-        hostname = None
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.settimeout(3)
+            result = sock.connect_ex((ip, port))
+            if result == 0:
+                open_ports.append(port)
 
     if verbose:
-        if hostname is None:
+        hostname = get_hostname(ip)
+        if hostname:
+            return f"Open ports for {hostname} ({ip})\nPORT     SERVICE\n" + "\n".join([f"{port:<9}{socket.getservbyport(port)}" for port in open_ports])
+        else:
             return f"Open ports for {ip}\nPORT     SERVICE\n" + "\n".join([f"{port:<9}{socket.getservbyport(port)}" for port in open_ports])
-        return f"Open ports for {hostname} ({ip})\nPORT     SERVICE\n" + "\n".join([f"{port:<9}{socket.getservbyport(port)}" for port in open_ports])
+    
     return open_ports
+
+def get_hostname(ip):
+    try:
+        return socket.gethostbyaddr(ip)[0]
+    except socket.error:
+        return None
