5e225861a0078587f4ffb7dca9b56cba0d347771
creinis <115181527+creinis@users.noreply.github.com>
2024-03-05 08:53:17 +0100

    add: american british translator

---
+++ 07_Quality_Assurance_Certification/Cert05_American-British_Translator/README.md
@@ -0,0 +1,3 @@
+# American British Translator
+
+This is the boilerplate for the American British Translator project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/quality-assurance/quality-assurance-projects/american-british-translator
---
+++ 07_Quality_Assurance_Certification/Cert05_American-British_Translator/public/index.js
@@ -0,0 +1,30 @@
+
+const translateHandler = async () => {
+  const textArea = document.getElementById("text-input");
+  const localeArea = document.getElementById("locale-select");
+  const errorArea = document.getElementById("error-msg");
+  const translatedArea = document.getElementById("translated-sentence");
+  
+  errorArea.innerText = "";
+  translatedArea.innerText = "";
+
+  const data = await fetch("/api/translate", {
+    method: "POST",
+    headers: {
+      "Accept": "application/json",
+      "Content-type": "application/json"
+    },
+    body: JSON.stringify({"text": textArea.value, "locale": localeArea.value})
+  });
+
+  const parsed = await data.json();
+  if (parsed.error) {
+    errorArea.innerText = JSON.stringify(parsed);
+    return;
+  }
+
+  translatedArea.innerHTML = parsed.translation;
+  return;
+};
+
+document.getElementById("translate-btn").addEventListener("click", translateHandler)
