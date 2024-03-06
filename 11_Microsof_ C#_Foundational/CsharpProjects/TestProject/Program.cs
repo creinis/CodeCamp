fa571e826c7fbd6095d1b01ee2abe2cb45ce4fb9
creinis <115181527+creinis@users.noreply.github.com>
2024-03-06 15:41:12 +0100

    test project dice

--- 11_Microsof_ C#_Foundational/CsharpProjects/TestProject/Program.cs
+++ 11_Microsof_ C#_Foundational/CsharpProjects/TestProject/Program.cs
@@ -1,3 +1,9 @@
 ﻿Random dice = new Random();
-int roll = dice.Next(1, 7);
-Console.WriteLine(roll);
+int roll1 = dice.Next();
+int roll2 = dice.Next(101);
+int roll3 = dice.Next(50, 101);
+
+Console.Clear();
+Console.WriteLine($"First roll: {roll1}");
+Console.WriteLine($"Second roll: {roll2}");
+Console.WriteLine($"Third roll: {roll3}");
