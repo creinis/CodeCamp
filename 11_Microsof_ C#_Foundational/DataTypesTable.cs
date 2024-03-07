fab1c5d64671b186bc39c74409b42748a8708c0b
creinis <115181527+creinis@users.noreply.github.com>
2024-03-07 16:21:00 +0100

    data type table of content

---
+++ 11_Microsof_ C#_Foundational/DataTypesTable.cs
@@ -0,0 +1,15 @@
+Console.Clear();
+Console.WriteLine("Data types:\n");
+Console.WriteLine("Signed integral types:");
+
+Console.WriteLine($"sbyte  : \t\t{sbyte.MinValue} to {sbyte.MaxValue}");
+Console.WriteLine($"short  : \t\t{short.MinValue} to {short.MaxValue}");
+Console.WriteLine($"int    : \t\t{int.MinValue} to {int.MaxValue}");
+Console.WriteLine($"long   : \t\t{long.MinValue} to {long.MaxValue}\n");
+
+Console.WriteLine("Unsigned integral types:");
+
+Console.WriteLine($"byte   : \t\t{byte.MinValue} to {byte.MaxValue}");
+Console.WriteLine($"ushort : \t\t{ushort.MinValue} to {ushort.MaxValue}");
+Console.WriteLine($"uint   : \t\t{uint.MinValue} to {uint.MaxValue}");
+Console.WriteLine($"ulong  : \t\t{ulong.MinValue} to {ulong.MaxValue}\n");
