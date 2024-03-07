09c878e378fdb3e118b7d441fe6e1c688f42d4a0
creinis <115181527+creinis@users.noreply.github.com>
2024-03-07 16:36:54 +0100

    update studies

--- 11_Microsof_ C#_Foundational/DataTypesTable.cs
+++ 11_Microsof_ C#_Foundational/DataTypesTable.cs
@@ -13,3 +13,11 @@ Console.WriteLine($"byte   : \t\t{byte.MinValue} to {byte.MaxValue}");
 Console.WriteLine($"ushort : \t\t{ushort.MinValue} to {ushort.MaxValue}");
 Console.WriteLine($"uint   : \t\t{uint.MinValue} to {uint.MaxValue}");
 Console.WriteLine($"ulong  : \t\t{ulong.MinValue} to {ulong.MaxValue}\n");
+
+Console.WriteLine("Floating point types:");
+Console.WriteLine($"float  : \t\t{float.MinValue} to {float.MaxValue} ");
+Console.WriteLine("\t\t\twith ~6-9 digits of precision\n");
+Console.WriteLine($"double : \t\t{double.MinValue} to {double.MaxValue}");
+Console.WriteLine("\t\t\twith ~15-17 digits of precision\n");
+Console.WriteLine($"decimal: \t\t{decimal.MinValue} to {decimal.MaxValue}");
+Console.WriteLine("\t\t\twith 28-29 digits of precision\n");
