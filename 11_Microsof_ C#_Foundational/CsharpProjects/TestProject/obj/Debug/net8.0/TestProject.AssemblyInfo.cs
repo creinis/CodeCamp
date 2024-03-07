f06a505227a9bc10a0bf92f438491fab112122e3
creinis <115181527+creinis@users.noreply.github.com>
2024-03-07 11:37:58 +0100

    challenge 01

--- 11_Microsof_ C#_Foundational/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ 11_Microsof_ C#_Foundational/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd00e4523d6666fba1940ff76e0a37359083fd34")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+963d890c56f3341299de381d41e9ce294a7e6810")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
