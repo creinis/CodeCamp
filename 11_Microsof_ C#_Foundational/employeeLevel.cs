01b784c5863d8bbdbb78fb9baf136389e723e474
creinis <115181527+creinis@users.noreply.github.com>
2024-03-07 14:46:22 +0100

    C# exercises update

---
+++ 11_Microsof_ C#_Foundational/employeeLevel.cs
@@ -0,0 +1,25 @@
+int employeeLevel = 200;
+string employeeName = "John Smith";
+
+string title = "";
+
+switch (employeeLevel)
+{
+    case 100:
+        title = "Junior Associate";
+        break;
+    case 200:
+        title = "Senior Associate";
+        break;
+    case 300:
+        title = "Manager";
+        break;
+    case 400:
+        title = "Senior Manager";
+        break;
+    default:
+        title = "Associate";
+        break;
+}
+
+Console.WriteLine($"{employeeName}, {title}");
