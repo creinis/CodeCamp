cbaabd0a202414a95ea6d8a559143b17c565cbd1
creinis <115181527+creinis@users.noreply.github.com>
2024-03-08 13:28:32 +0100

    ex02

--- 11_Microsof_ C#_Foundational/parseInt.cs
+++ 11_Microsof_ C#_Foundational/parseInt.cs
@@ -1,5 +1,7 @@
 string value = "102";
 int result = 0;
+Console.Clear();
+Console.WriteLine("Ex 01:\n");
 if (int.TryParse(value, out result))
 {
    Console.WriteLine($"Measurement: {result}");
@@ -8,7 +10,29 @@ else
 {
    Console.WriteLine("Unable to report the measurement.");
 }
-Console.WriteLine($"Measurement (w/ offset): {50 + result}");
+Console.WriteLine($"Measurement (w/ offset): {50 + result}\n");
 
 // The int.TryParse() method returns true if it successfully converted the string variable value into an int; 
 // otherwise, it returns false. So, surround the statement in an if statement, and then perform the decision logic, accordingly.
+
+
+string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+
+decimal total = 0m;
+string message = "";
+Console.WriteLine("Ex 02:\n");
+foreach (var value in values)
+{
+    decimal number; // stores the TryParse "out" value
+    if (decimal.TryParse(value, out number))
+    {
+        total += number;
+    } else
+    {
+        message += value;
+    }
+}
+
+Console.WriteLine($"Message: {message}");
+Console.WriteLine($"Total: {total}");
+
