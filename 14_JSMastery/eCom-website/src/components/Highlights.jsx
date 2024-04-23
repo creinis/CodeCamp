d4ec375fa12e4d8929bb02b3fa9173c49ec4984c
creinis <115181527+creinis@users.noreply.github.com>
2024-04-23 21:05:36 +0200

    component Highlights almost done

--- 14_JSMastery/eCom-website/src/components/Highlights.jsx
+++ 14_JSMastery/eCom-website/src/components/Highlights.jsx
@@ -1,10 +1,29 @@
-
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { watchImg, rightImg } from '../utils';
 
 const Highlights = () => {
+
+  useGSAP(() => {
+    gsap.to('#title', { opacity: 1, y: 0 })
+    gsap.to('.link', { opacity: 1, y: 0, duration: 1, stagger: 0.25 })
+  },[])
   return (
-    <div>
-      
-    </div>
+    <section id='highlights' className='w-screen overflow-hidden h-full common-padding bg-zinc'>
+      <div className='screen-max-width'>
+        <div className='mb-12 w-full items-end md:flex justify-between'>
+          <h1 id='title' className='section-heading' >Get the highlights.</h1>
+          <div className='flex flex-wrap items-end gap-5'>
+            <p className='link'>Watch the film
+              <img src={watchImg} alt='watch' className='ml-2'/>
+            </p>
+            <p className='link'>Watch the event
+              <img src={rightImg} alt='right' className='ml-2'/>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
