96666e5b7392f839fd2bee55bcb4c4ccf8e72acc
creinis <115181527+creinis@users.noreply.github.com>
2024-04-23 15:31:23 +0200

    stagger

--- 14_JSMastery/Gsap/src/pages/GsapStagger.jsx
+++ 14_JSMastery/Gsap/src/pages/GsapStagger.jsx
@@ -1,6 +1,24 @@
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+
 const GsapStagger = () => {
   // TODO: Implement the gsap.stagger() method
-
+useGSAP(() => {
+  gsap.to('.stagger-box', {
+    y: 250,
+    rotation: 360,
+    borderRadius: '100%',
+    repeat: -1,
+    yoyo: true,
+    stagger: {
+      amount: 1.5,
+      grid: [2, 1],
+      axis: 'y',
+      ease: 'circ.inOut',
+      from: 'center',
+    }
+  })
+})
   return (
     <main>
       <h1>GsapStagger</h1>
