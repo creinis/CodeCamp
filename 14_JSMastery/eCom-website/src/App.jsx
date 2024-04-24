caa7c0057036e9ddd25ded4bf69936a13af89eb4
creinis <115181527+creinis@users.noreply.github.com>
2024-04-24 14:36:23 +0200

    component Model create

--- 14_JSMastery/eCom-website/src/App.jsx
+++ 14_JSMastery/eCom-website/src/App.jsx
@@ -1,16 +1,18 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Highlights from './components/Highlights';
+import Model from './components/Model';
 
 const App = () => {
 
   return (
     <main className='bg-black'>
       <Navbar/>
-
       <Hero/>
-
       <Highlights/>
+      <Model/>
+      
+
 
     </main>
   )
---
+++ 14_JSMastery/eCom-website/src/components/Model.jsx
@@ -0,0 +1,12 @@
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+
+const Model = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Model
